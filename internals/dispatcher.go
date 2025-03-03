cded14d4d5e62b2a2482c8c5c21050911c37df12
yuri <yohannesh.2018@gmail.com>
2025-03-03 11:40:59 +0300

    dispatcher done

---
+++ internals/dispatcher.go
@@ -0,0 +1,97 @@
+package internals
+
+import (
+	"fmt"
+	"go-fe-fwk/pkgs/utils"
+)
+
+type Dispatcher struct {
+	Subs          map[string][]func(payload ...any) `json:"subs"`
+	AfterHandlers []func(payload ...any)            `json:"afterHandlers"`
+}
+
+func (dispatcher *Dispatcher) Subscribe(commandName string, handler func(payload ...any)) func() {
+	if _, ok := dispatcher.Subs[commandName]; !ok {
+		dispatcher.Subs[commandName] = []func(payload ...any){}
+	}
+	handlers := dispatcher.Subs[commandName]
+	if utils.FuncExists(handlers, handler) {
+		return func() {}
+	}
+	handlers = append(handlers, handler)
+	return func() {
+		idx := utils.IndexOfFunction(handlers, handler)
+		handlers = append(handlers[:idx], handlers[idx+1:]...)
+	}
+}
+
+func (dispatcher *Dispatcher) AfterEveryCommand(handler func(payload ...any)) func() {
+	dispatcher.AfterHandlers = append(dispatcher.AfterHandlers, handler)
+	return func() {
+		idx := utils.IndexOfFunction(dispatcher.AfterHandlers, handler)
+		dispatcher.AfterHandlers = append(dispatcher.AfterHandlers[:idx], dispatcher.AfterHandlers[idx+1:]...)
+	}
+}
+
+func (dispatcher *Dispatcher) dispatch(commandName string, payload any) {
+	if _, ok := dispatcher.Subs[commandName]; ok {
+		for _, handler := range dispatcher.Subs[commandName] {
+			handler(payload)
+		}
+	} else {
+		fmt.Println("there is no command by that name")
+	}
+	for _, handler := range dispatcher.AfterHandlers {
+		handler()
+	}
+}
+
+//     dispatch(commandName, payload){
+//         if(this.#subs.has(commandName)){
+//             this.#subs.get(commandName).forEach((handler)=>{
+//                 handler(payload)
+//             })
+//         }else{
+//             console.log(`there is no command by the name ${commandName}`)
+//         }
+
+//         this.#afterHandlers.forEach((handler)=>{handler()})
+//     }
+// export class Dispatcher {
+//     #subs = new Map()
+//     #afterHandlers =[]
+//     subscribe(commandName, handler){
+//         if(!this.#subs.has(commandName)){
+//             this.#subs.set(commandName, [])
+//         }
+//         const handlers = this.#subs.get(commandName)
+//         if(handlers.includes(handler)){
+//             return ()=>{}
+//         }
+//         handlers.push(handler)
+//         return ()=>{
+//             const idx = handlers.indexOf(handler)
+//             handlers.splice(idx,1)
+//         }
+//     }
+//     afterEveryCommand(handler) {
+//         this.#afterHandlers.push(handler)
+//         return () =>{
+//             const idx= this.#afterHandlers.indexOf(handler)
+//             this.#afterHandlers.splice(idx,1)
+//         }
+//     }
+
+//     dispatch(commandName, payload){
+//         if(this.#subs.has(commandName)){
+//             this.#subs.get(commandName).forEach((handler)=>{
+//                 handler(payload)
+//             })
+//         }else{
+//             console.log(`there is no command by the name ${commandName}`)
+//         }
+
+//         this.#afterHandlers.forEach((handler)=>{handler()})
+//     }
+
+// }
--- pkgs/utils/array.go
+++ pkgs/utils/array.go
@@ -1,5 +1,7 @@
 package utils
 
+import "reflect"
+
 // func WithoutNulls(arr []internals.Vdom) []internals.Vdom {
 // 	var newArr = make([]internals.Vdom, 0, len(arr))
 // 	for _, value := range arr {
@@ -11,3 +13,20 @@ package utils
 //		return newArr
 //	}
 func WithoutNulls() {}
+func FuncExists(funcArray []func(payload ...any), target func(payload ...any)) bool {
+	for _, f := range funcArray {
+		if reflect.ValueOf(f).Pointer() == reflect.ValueOf(target).Pointer() {
+			return true
+		}
+	}
+	return false
+}
+
+func IndexOfFunction(arr []func(payload ...any), target func(payload ...any)) int {
+	for i, f := range arr {
+		if reflect.ValueOf(f).Pointer() == reflect.ValueOf(target).Pointer() {
+			return i
+		}
+	}
+	return -1
+}
